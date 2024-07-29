d2438a5aa61eba833179779e1a3bf972dfc0ce14
李千千 <851374695@qq.com>
2024-07-29 16:37:12 +0800

    feat: driverjs 基本使用

---
+++ demo/driverjs/driverjs-basic-demo/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import AnimatedTour from './components/AnimatedTour.vue'
+</script>
+
+<template>
+  <AnimatedTour />
+</template>
+
+<style scoped></style>
---
+++ demo/driverjs/driverjs-basic-demo/src/components/AnimatedTour.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="animated-tour">
+    <span class="line">第一</span>
+    <span class="line">第二</span>
+    <span class="line">第三</span>
+    <span class="line">第四</span>
+    <span class="line">第五</span>
+    <span class="line">第六</span>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import { driver as Driver } from 'driver.js'
+import 'driver.js/dist/driver.css'
+
+onMounted(() => {
+  const driver = Driver({
+    showProgress: true,
+    steps: [
+      {
+        element: '.animated-tour',
+        popover: {
+          title: '新手导航',
+          description: '这是新手导航',
+          side: 'left',
+          align: 'start',
+        },
+      },
+      {
+        element: '.line:nth-child(1)',
+        popover: {
+          title: '新手导航',
+          description: '第一步',
+          side: 'bottom',
+          align: 'start',
+        },
+      },
+      {
+        element: '.line:nth-child(2)',
+        popover: {
+          title: '新手导航',
+          description: '第二步',
+          side: 'right',
+          align: 'end',
+        },
+      },
+      {
+        element: '.line:nth-child(3)',
+        popover: {
+          title: '新手导航',
+          description: '第三步',
+          side: 'right',
+          align: 'end',
+        },
+      },
+      {
+        element: '.line:nth-child(4)',
+        popover: {
+          title: '新手导航',
+          description: '第四步',
+          side: 'right',
+          align: 'end',
+        },
+      },
+      {
+        element: '.line:nth-child(5)',
+        popover: {
+          title: '新手导航',
+          description: '第五步',
+          side: 'right',
+          align: 'end',
+        },
+      },
+      {
+        element: '.line:nth-child(6)',
+        popover: {
+          title: '新手导航',
+          description: '第六步',
+          side: 'right',
+          align: 'start',
+        },
+      },
+    ],
+  })
+  driver.drive()
+})
+</script>
+
+<style scoped>
+.animated-tour {
+  width: 200px;
+  display: flex;
+  flex-direction: column;
+}
+.line {
+  margin-bottom: 20px;
+  background-color: orange;
+}
+</style>
