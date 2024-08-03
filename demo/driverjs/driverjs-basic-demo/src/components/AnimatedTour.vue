d7d36977d8dc3b8a8c35a27902192b1f215750cc
李千千 <851374695@qq.com>
2024-08-03 13:33:56 +0800

    feat: add Driver.js、SpinKit、Timesheet

--- demo/driverjs/driverjs-basic-demo/src/components/AnimatedTour.vue
+++ demo/driverjs/driverjs-basic-demo/src/components/AnimatedTour.vue
@@ -13,76 +13,75 @@
 import { onMounted } from 'vue'
 import { driver as Driver } from 'driver.js'
 import 'driver.js/dist/driver.css'
-
+const driver = Driver({
+  showProgress: true,
+  steps: [
+    {
+      element: '.animated-tour',
+      popover: {
+        title: '新手导航',
+        description: '这是新手导航',
+        side: 'left',
+        align: 'start',
+      },
+    },
+    {
+      element: '.line:nth-child(1)',
+      popover: {
+        title: '新手导航',
+        description: '第一步',
+        side: 'bottom',
+        align: 'start',
+      },
+    },
+    {
+      element: '.line:nth-child(2)',
+      popover: {
+        title: '新手导航',
+        description: '第二步',
+        side: 'right',
+        align: 'end',
+      },
+    },
+    {
+      element: '.line:nth-child(3)',
+      popover: {
+        title: '新手导航',
+        description: '第三步',
+        side: 'right',
+        align: 'end',
+      },
+    },
+    {
+      element: '.line:nth-child(4)',
+      popover: {
+        title: '新手导航',
+        description: '第四步',
+        side: 'right',
+        align: 'end',
+      },
+    },
+    {
+      element: '.line:nth-child(5)',
+      popover: {
+        title: '新手导航',
+        description: '第五步',
+        side: 'right',
+        align: 'end',
+      },
+    },
+    {
+      element: '.line:nth-child(6)',
+      popover: {
+        title: '新手导航',
+        description: '第六步',
+        side: 'right',
+        align: 'start',
+      },
+    },
+  ],
+})
 onMounted(() => {
-  const driver = Driver({
-    showProgress: true,
-    steps: [
-      {
-        element: '.animated-tour',
-        popover: {
-          title: '新手导航',
-          description: '这是新手导航',
-          side: 'left',
-          align: 'start',
-        },
-      },
-      {
-        element: '.line:nth-child(1)',
-        popover: {
-          title: '新手导航',
-          description: '第一步',
-          side: 'bottom',
-          align: 'start',
-        },
-      },
-      {
-        element: '.line:nth-child(2)',
-        popover: {
-          title: '新手导航',
-          description: '第二步',
-          side: 'right',
-          align: 'end',
-        },
-      },
-      {
-        element: '.line:nth-child(3)',
-        popover: {
-          title: '新手导航',
-          description: '第三步',
-          side: 'right',
-          align: 'end',
-        },
-      },
-      {
-        element: '.line:nth-child(4)',
-        popover: {
-          title: '新手导航',
-          description: '第四步',
-          side: 'right',
-          align: 'end',
-        },
-      },
-      {
-        element: '.line:nth-child(5)',
-        popover: {
-          title: '新手导航',
-          description: '第五步',
-          side: 'right',
-          align: 'end',
-        },
-      },
-      {
-        element: '.line:nth-child(6)',
-        popover: {
-          title: '新手导航',
-          description: '第六步',
-          side: 'right',
-          align: 'start',
-        },
-      },
-    ],
-  })
   driver.drive()
 })
 </script>
